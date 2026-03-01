21ab2e50cbc1dbbf61631794774997917d435a68
Xilin Sun <s.sn.giraffe@gmail.com>
2026-03-01 12:52:13 -0800

    Setup linting and formatting tools (Patch 02)

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  { ignores: ['dist/', 'functions/', 'node_modules/'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  eslintConfigPrettier,
+)
